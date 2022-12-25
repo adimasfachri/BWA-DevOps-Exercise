3a4ef22bbb9ad9aac8df08cea7b6726fde38f332
Adimas Fachri <adimas_fachri@yahoo.com>
2022-12-25 15:52:55 +0700

    change to spesific server

--- loadbalancer/server2.js
+++ loadbalancer/server2.js
@@ -6,7 +6,7 @@ conist port = 3002;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  res.end('Hi, This is Server 2!\n');
 });
 
 server.listen(port, hostname, () => {
--- loadbalancer/server3.js
+++ loadbalancer/server3.js
@@ -6,7 +6,7 @@ const port = 3003;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  res.end('Hi, This is Server 3!\n');
 });
 
 server.listen(port, hostname, () => {
